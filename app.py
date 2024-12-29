dc438d24cb9af3ba5fdddfe826942c815cfc860b
ANGEL MORENO TORRES <newcastilian@gmail.com>
2024-12-29 11:46:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from estrategia_trading import EstrategiaTrading  # Asegúrate de importar tu clase correctamente
 
 # Configuración inicial de la página
